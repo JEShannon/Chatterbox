caa98ab1a0f4979b41ca04eb761a034abd16571d
JEShannon <James.Evan.Shannon@gmail.com>
2024-01-02 13:04:46 -0500

    Added base model framework.  Implementing GPT4 is next

---
+++ ModelChatter.py
@@ -0,0 +1,47 @@
+#This model is to hold the main class that all chatterboxes use.
+
+class chatterbox ():
+    #Functions for setting the context for the bot and initialization
+    def addContext(self):
+        pass
+
+    def setContext(self):
+        pass
+
+    def getContext(self):
+        pass
+
+    def useContext(self, context):
+        pass
+
+    def setKey(self, key, keyName):
+        pass
+
+    def getKey(self):
+        pass
+
+    def useKey(self):
+        pass
+
+    def initialize(self):
+        pass
+
+    def isInitialized(self):
+        pass
+
+    def reInitialize(self):
+        pass
+
+    #Functions for interacting with the bot
+    def respond(self):
+        pass
+
+    def prompt(self, text):
+        pass
+
+    def getTranscript(self):
+        pass
+
+    def updateTranscript(self):
+        pass
+    
