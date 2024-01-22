05accc3cd84c1c1d276332bf1db4ef90ddb2ac72
JEShannon <James.Evan.Shannon@gmail.com>
2024-01-22 17:53:57 -0500

    Initial testing file made for early stage demonstration and trials of the library

---
+++ gptTest.py
@@ -0,0 +1,8 @@
+from .GPTChatter import *
+
+#Just give it a singulat test, see if it can respond correctly when asked to perform a simple task.
+
+myPrompt = ["system : Please respond by only writing the following three letters in the NATO Phonetic Alphabet: A B C"]
+
+#A correct response is first any response, and then secondly the text should read "Alpha Bravo Charlie"
+
