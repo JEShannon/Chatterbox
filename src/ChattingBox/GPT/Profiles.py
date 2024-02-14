dfbce89430327e95773527679810f8c39d4aafab
JEShannon <James.Evan.Shannon@gmail.com>
2024-02-14 14:01:44 -0500

    text to script function framework

--- src/ChattingBox/GPT/Profiles.py
+++ src/ChattingBox/GPT/Profiles.py
@@ -1,4 +1,9 @@
 
+def TEXT_TO_SCRIPT(text):
+    #this function takes normal sentence by sentence text and converts it to a valid script
+    #it is intended that any script returned by this function will pass validateContext()
+    pass
+
 class AgentProfile:
     def __init__(self, name, desc, script):
         self.name = name
@@ -7,3 +12,7 @@ class AgentProfile:
 
     def getScript(self):
         return self.__script.copy()
+
+
+_SecondOpinionScript = 
+
