d030c2b5afdc12af0170ab28590f2a02f57b925e
JEShannon <James.Evan.Shannon@gmail.com>
2024-03-08 15:21:03 -0500

    Added runfile for the GPT tests.  Need to finish test writing and validate the tests all pass correctly

---
+++ GPTfunctionTest.py
@@ -0,0 +1,14 @@
+import src.ChattingBox.GPTChatter as gptchat
+import test.ChattingBox.gptTest as gpt_test
+
+
+#The point of this file is to run the tests located in the tests/ChattingBox/gptTest.py
+def runTests():
+    testsRun = 0
+    testsFail = 0
+    #run
+    pass
+
+
+if __name__ == "__main__":
+    runTests()
